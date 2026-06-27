77c72f28bc74b0e20af77ec50ec133a518c0f099
Zunaid Hossain <zunaidhossain.india@gmail.com>
2026-06-28 03:16:51 +0530

    Initial Spring Boot Project Set-up

---
+++ src/main/java/com/shippex/ShippexApplication.java
@@ -0,0 +1,13 @@
+package com.shippex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippexApplication.class, args);
+	}
+
+}
